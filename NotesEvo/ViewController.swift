36865d0047ec57a6bfc8a7947915deb0a30c7be5
Andrey Boychuk <andrewboychuk@gmail.com>
2019-05-20 11:18:36 +0300

    Project initial commit.

---
+++ NotesEvo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NotesEvo
+//
+//  Created by Andrew Boychuk on 5/17/19.
+//  Copyright © 2019 Andrew Boychuk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
