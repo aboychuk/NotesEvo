3d4a872d9ad4535af24bca4198a31e42f2caa081
Andrey Boychuk <andrewboychuk@gmail.com>
2019-06-12 10:14:10 +0300

    Readme.md added.

--- NotesEvo/Supporting Files/Library/Extensions/UITableView+Extensions.swift
+++ NotesEvo/Supporting Files/Library/Extensions/UITableView+Extensions.swift
@@ -20,15 +20,21 @@ extension UITableView {
         return cell
     }
     
+    ///Use this method if ios version is < 11, else use performBatchUpdate.
     func updateTableWith(block: () -> ()) {
         self.beginUpdates()
         block()
         self.endUpdates()
     }
     
-    func registerCell<T>(type: T.Type) {
+    /// Registers a nib object containing a cell with the table view under a specified identifier if nib is nil, registers a class for use in creating new table cells.
+    func registerCell<T: UITableViewCell>(type: T.Type) {
         let cellName = typeString(type)
-        let nib = UINib(nibName: cellName, bundle: .main)
-        self.register(nib, forCellReuseIdentifier: cellName)
+        let nib = UINib.nib(from: type, bundle: .main)
+        if nib == nil {
+            self.register(type, forCellReuseIdentifier: cellName)
+        } else {
+            self.register(nib, forCellReuseIdentifier: cellName)
+        }
     }
 }
--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # NotesEvo
+
+## Used
+Language - Swift\
+Architecture - Model View Controller\
+Min version OS - 11.0\
+Storage - Core Data
+
+## Features
+Create and display notes\
+Persistent storage\
+Note editing\
+Note deletion\ 
+Search by notes\
+Pagination\
+Sorting by date\
